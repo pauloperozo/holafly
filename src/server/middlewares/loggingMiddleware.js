34ba6ae533ce36ef7465f263375322a2c853f50b
pauloperozo <pauloperozocolombia@gmail.com>
2023-04-15 23:10:51 -0500

    feat (middlewar) : create logs, edit ip

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,10 +1,16 @@
-const loggingMiddleware = (db) =>
-    (req, res, next) => {
-        const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        const headers = JSON.stringify(req.headers);
-        const originalUrl = req.originalUrl;
-        // Persist this info on DB
-        next();
-    }
+//////////////////////////////////////////////////////////////////////////////
+const loggingMiddleware = ( db ) => ( req, res, next ) => {
 
-module.exports = loggingMiddleware;
+    const logs = {
+        ip : req.headers['host'].split(':')[0].trim(),
+        header : JSON.stringify(req.headers),
+        action : req.originalUrl    
+    }
+    
+    db.logging.create(  logs  )
+    next()
+
+}
+//////////////////////////////////////////////////////////////////////////////
+module.exports = loggingMiddleware
+//////////////////////////////////////////////////////////////////////////////
